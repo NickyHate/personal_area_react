69382de9f75ebfb96f624f86cc41242fd3463c8e
Никита <nikitostokarev22@gmail.com>
2020-08-05 22:39:50 +0300

    Updates

---
+++ precache-manifest.30c99c7f60245d2e6beedc44185d4577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc69569fb9e72fc6d8e83b89e10bbf2b",
+    "url": "/personal_area_react/index.html"
+  },
+  {
+    "revision": "cd37fdafa03fa4ba0cd3",
+    "url": "/personal_area_react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "831dc71bd42d45c0a313",
+    "url": "/personal_area_react/static/css/main.0fff025d.chunk.css"
+  },
+  {
+    "revision": "cd37fdafa03fa4ba0cd3",
+    "url": "/personal_area_react/static/js/2.f72c819c.chunk.js"
+  },
+  {
+    "revision": "bce59f49097f33fd9c6194c2c4f8dc0b",
+    "url": "/personal_area_react/static/js/2.f72c819c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831dc71bd42d45c0a313",
+    "url": "/personal_area_react/static/js/main.bac247f0.chunk.js"
+  },
+  {
+    "revision": "bfbbd88fae6062343d2a",
+    "url": "/personal_area_react/static/js/runtime-main.75d28f6a.js"
+  }
+]);
