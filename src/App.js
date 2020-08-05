b6b7f59b8483fecae9370bddb4fbda939651de30
Никита <nikitostokarev22@gmail.com>
2020-08-05 22:41:50 +0300

    fix state

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
     this.state = {
       email: "",
       password: "",
-      hasAccount: true,
+      hasAccount: false,
       key: "",
       value: "",
       name: "",
